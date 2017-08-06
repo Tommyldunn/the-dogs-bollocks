b4afb1e34acbf33c961a0921f7426a3d38847a87
tommyldunn <tommylogandunn@gmail.com>
2017-08-06 17:54:45 -0400

    my defaults

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,9 +1,8 @@
 import { Component } from '@angular/core';
 
 @Component({
-  selector: 'app-root',
-  templateUrl: './app.component.html',
-  styleUrls: ['./app.component.scss']
+  selector: 'site-root',
+  templateUrl: './app.component.html'
 })
 export class AppComponent {
   title = 'app';
