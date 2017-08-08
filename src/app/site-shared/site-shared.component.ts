ce8b7b4b60ad47ad2236c378e9918283a1392b9b
tommyldunn <tommylogandunn@gmail.com>
2017-08-08 02:28:03 -0400

    cleaning

--- src/app/site-shared/site-shared.component.ts
+++
@@ -1,9 +0,0 @@
-import { Component } from '@angular/core';
-
-@Component({
-    moduleId: module.id,
-    selector: 'site-shared',
-    templateUrl: 'site-shared.component.html',
-    styleUrls: ['site-shared.component.css']
-})
-export class SiteSharedComponent { }
