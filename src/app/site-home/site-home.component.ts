8c0521f48806687f0fd1cd0bf5564becad1b5230
tommyldunn <tommylogandunn@gmail.com>
2017-08-07 22:45:25 -0400

    hero

--- src/app/site-home/site-home.component.ts
+++ src/app/site-home/site-home.component.ts
@@ -2,6 +2,7 @@ import { Component } from '@angular/core';
 
 @Component({
   selector: 'site-home',
-  templateUrl: 'site-home.component.html'
+  templateUrl: 'site-home.component.html',
+  styleUrls: ['site-home.component.scss']
 })
 export class SiteHomeComponent { }
