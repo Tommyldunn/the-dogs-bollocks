c6de1ac4b1478b0868bf07cf3d680a8647216dbe
tommyldunn <tommylogandunn@gmail.com>
2017-08-11 16:40:08 -0400

    autoprefixer

--- e2e/app.e2e-spec.ts
+++ e2e/app.e2e-spec.ts
@@ -1,6 +1,6 @@
 import { TheDogsBollocksPage } from './app.po';
 
-describe('the-dogs-bollocks App', () => {
+describe('the-dogs-bollocks Site', () => {
   let page: TheDogsBollocksPage;
 
   beforeEach(() => {
