58c59a0b89089e3630c8df9a44cbf34f308048df
tommyldunn <tommylogandunn@gmail.com>
2017-09-05 23:46:12 -0400

    Delancey

--- src/api/in-memory-data.service.ts
+++ src/api/in-memory-data.service.ts
@@ -5,12 +5,12 @@ export class InMemoryDataService {
     const shows = [
       {
         id: 1,
-        venueImage: '/assets/images/carra.png',
+        venueImage: '/assets/images/thedelancey.png',
         venueImage_class: 'black',
-        venueName: 'Carraghers',
-        venueLink: 'http://carraghersnyc.com/',
-        venueAddress: '228 W 39th St, New York, NY 10018',
-        date: 'Sunday Aug 27'
+        venueName: 'The Delancey',
+        venueLink: 'http://thedelancey.com/',
+        venueAddress: '168 Delancey St, New York, NY 10002 ',
+        date: 'Saturday Sep 9'
       }, {
         id: 2,
         venueImage: '/assets/images/arlenesgrocery.jpg',
@@ -19,14 +19,6 @@ export class InMemoryDataService {
         venueLink: 'http://www.arlenesgrocery.net/',
         venueAddress: '95 Stanton St. NYC',
         date: 'Saturday Sep 16'
-      }, {
-        id: 3,
-        venueImage: '/assets/images/carra.png',
-        venueImage_class: 'black',
-        venueName: 'Carraghers',
-        venueLink: 'http://carraghersnyc.com/',
-        venueAddress: '228 W 39th St, New York, NY 10018',
-        date: 'Sunday Oct 1'
       }, {
         id: 4,
         venueImage: '/assets/images/carra.png',
