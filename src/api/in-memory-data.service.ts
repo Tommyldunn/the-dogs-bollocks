cf9d62818ae015f063f314d08375279be98ba11e
tommyldunn <tommylogandunn@gmail.com>
2017-10-13 17:28:21 -0400

    added show

--- src/api/in-memory-data.service.ts
+++ src/api/in-memory-data.service.ts
@@ -5,21 +5,22 @@ export class InMemoryDataService {
     const shows = [
       {
         id: 1,
-        venueImage: '/assets/images/arlenesgrocery.jpg',
+        venueImage: '/assets/images/AlphabetLounge.png',
         venueImage_class: 'black',
-        venueName: 'Arlenes Grocery',
-        venueLink: 'http://www.arlenesgrocery.net/',
-        venueAddress: '95 Stanton St. NYC',
-        date: 'Saturday Sep 16'
-      }, {
-        id: 2,
-        venueImage: '/assets/images/carra.png',
-        venueImage_class: 'black',
-        venueName: 'Carraghers',
-        venueLink: 'http://carraghersnyc.com/',
-        venueAddress: '228 W 39th St, New York, NY 10018',
-        date: 'Sunday Oct 14'
-      }
+        venueName: 'Alphabet Lounge',
+        venueLink: 'http://www.alphabetnyc.com/',
+        venueAddress: '104 AVENUE C, NYC, NY 10009',
+        date: 'Friday Oct 27'
+      },
+      // {
+      //   id: 2,
+      //   venueImage: '/assets/images/carra.png',
+      //   venueImage_class: 'black',
+      //   venueName: 'Carraghers',
+      //   venueLink: 'http://carraghersnyc.com/',
+      //   venueAddress: '228 W 39th St, New York, NY 10018',
+      //   date: 'Sunday Oct 14'
+      // }
     ];
     const members = [
       {
